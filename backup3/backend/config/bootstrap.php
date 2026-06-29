5ccd7bbca6aea27632281b32657ab8b90c9c160d
brunobetzler <brunobetzler@gmail.com>
2026-06-29 12:35:57 +0700

    Delete backup3 directory

--- backup3/backend/config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Định nghĩa ROOT_PATH (chỉ cần chạy 1 lần cho toàn bộ project)
-if (!defined('ROOT_PATH')) {
-    define('ROOT_PATH', dirname(__DIR__)); 
-    // ví dụ: C:/xampp/htdocs/hotel/hotel-api-php
-}
-
-// autoload Database
-require_once ROOT_PATH . '/config/Database.php';
